45a3a1c0a56fcb434c5215b2f1a9ed6dc387a9de
Sun <muzhaoyang2022@outlook.com>
2026-04-06 15:16:02 +0800

    adapt bazel build and bazel test in workflow

--- hello.cc
+++ hello.cc
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "math.h"
 
 int main() {
+    std::cout << add(1, 2) << std::endl;
     std::cout << "hello bazel" << std::endl;
     return 0;
 }
---
+++ math_test.cc
@@ -0,0 +1,8 @@
+#include "math.h"
+#include <cassert>
+
+int main() {
+    assert(add(1, 2) == 3);
+    assert(add(2, 3) == 5);
+    return 0;
+}
